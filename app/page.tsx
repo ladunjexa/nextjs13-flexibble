59ad7c850c2028ac2d5aeda23136b7d451f953d0
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-06-26 21:32:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,58 @@
-import React from "react";
+import { ProjectInterface, ProjectsSearch } from "@/common.types";
+import { fetchAllProjects } from "@/lib/actions";
+import { Categories, Pagination, ProjectCard } from "@/components/client";
 
-type Props = {};
+type SearchParams = {
+  category?: string;
+  endcursor?: string;
+};
 
-const Home = (props: Props) => {
+type Props = {
+  searchParams: SearchParams;
+};
+
+const Home = async (searchParams: Props) => {
+  const { category, endcursor } = searchParams?.searchParams || {};
+
+  const data = (await fetchAllProjects(category, endcursor)) as ProjectsSearch;
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        <Categories />
+        <p className="no-result-text text-center">
+          No projects found. Be the first to add a project!
+        </p>
+      </section>
+    );
+  }
+
+  const pagination = data?.projectSearch?.pageInfo;
+
   return (
     <section className="flex-start flex-col paddings mb-16">
-      <h1>Categories</h1>
-      <h1>Posts</h1>
-      <h1>LoadMore:</h1>
+      <Categories />
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
+      <Pagination
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
     </section>
   );
 };
