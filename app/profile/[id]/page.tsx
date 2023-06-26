1b3dbf024bc76259fe94b45aa0ea03b9ea2078ed
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-06-26 21:19:57 +0300

    Create page.tsx

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { UserProfile } from "@/common.types";
+import { getUserProjects } from "@/lib/actions";
+import ProfilePage from "@/components/server/ProfilePage";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const UserProfile = async ({ params }: Props) => {
+  const result = (await getUserProjects(params.id, 100)) as {
+    user: UserProfile;
+  };
+
+  if (!result?.user) {
+    return <p className="no-result-text">Failed to fetch user info</p>;
+  }
+  return <ProfilePage user={result?.user} />;
+};
+
+export default UserProfile;
