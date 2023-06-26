66b51d297c826e23f349fc071a0054f47df79eb0
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-06-26 21:31:47 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import Footer from "@/components/server/Footer";
+import Navbar from "@/components/server/Navbar";
 
 export const metadata = {
   title: "Flexibble",
