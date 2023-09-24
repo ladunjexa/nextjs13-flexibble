be4c34159ab2de5d0da9c3c57df1529846874e38
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-09-24 05:42:39 +0300

    Update index.ts

--- graphql/index.ts
+++ graphql/index.ts
@@ -67,6 +67,36 @@ export const deleteProjectMutation = `
   }
 `;
 
+export const allProjectsQuery = `
+	query getProjects($endCursor: String) {
+	  projectSearch(first: 8, after: $endCursor) {
+	    pageInfo {
+	      hasNextPage
+	      hasPreviousPage
+	      startCursor
+	      endCursor
+	    }
+	    edges {
+	      node {
+	        title
+	        githubUrl
+	        description
+	        liveSiteUrl
+	        id
+	        image
+	        category
+	        createdBy {
+	          id
+	          email
+	          name
+	          avatarUrl
+	        }
+	      }
+	    }
+	  }
+	}
+`;
+
 export const projectsQuery = `
   query getProjects($category: String, $endCursor: String) {
     projectSearch(first: 8, after: $endCursor, filter: {category: {eq: $category}}) {
